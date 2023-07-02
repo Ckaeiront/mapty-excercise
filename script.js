10e969372642c6a6b03eaa52964f5559d67e2687
Ckaeiront <stampedesw29@gmail.com>
2023-07-02 12:56:46 -0300

    testing

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (location) {
+      const { latitude, longitude } = location.coords;
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+      console.log(map);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      /*
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+      */
+
+      map.on('click', function (mev) {
+        let { lat, lng } = mev.latlng;
+        let clickCoordsArray = [lat, lng];
+        console.log(mev);
+        L.marker(clickCoordsArray).addTo(map).bindPopup('Workout').openPopup();
+      });
+    },
+    function (params) {
+      console.log('fodase ' + params);
+    }
+  );
+}
